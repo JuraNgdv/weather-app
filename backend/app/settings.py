586400a1984205031d38002bb8d684a4125c3c0b
Your Name <you@example.com>
2025-03-12 17:43:56 +0200

    cors problem

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -84,11 +84,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["*"]
 ROOT_URLCONF = 'app.urls'
 
