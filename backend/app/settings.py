7dcccd97406abf56835b1e983dc2401ccf30b0d4
Your Name <you@example.com>
2025-03-12 16:27:51 +0200

    docker.yml

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -86,10 +86,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
