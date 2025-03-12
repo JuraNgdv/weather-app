77b50a7f64ff21deb76fd714fb71afcf9a2d9214
Your Name <you@example.com>
2025-03-12 16:57:42 +0200

    docker.yml

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -166,6 +166,21 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_HEADERS = (
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Credentials',
+    )
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+ ]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
