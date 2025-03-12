3c4e982ffe28d4aa059653b1eaac703f08182d73
Your Name <you@example.com>
2025-03-12 18:55:27 +0200

    city search

--- frontend/src/components/WeatherList.jsx
+++ frontend/src/components/WeatherList.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import WeatherCard from './WeatherCard';
 
-const WeatherList = ({ weather }) => {
+const WeatherList = ({weather}) => {
     console.log(weather);
     console.log(weather.results);
     return (
         <div>
             <h2>Weather Information</h2>
-            {!weather.results && weather.length === 0 ? (
+            {weather.results && weather.results.length === 0 ? (
                 <p>No weather data available</p>
             ) : (
-
-                weather.results.map((card) => {
-                    <WeatherCard key={card.id} weatherData={card} />
-                })
+                <div className="weather-cards-container">
+                    {weather.results && weather.results.map((card) => {
+                        // Add a return statement here for WeatherCard
+                        return <WeatherCard key={card.id} card={card} />;
+                    })}
+                </div>
             )}
         </div>
     );
 };
+
+
 
 export default WeatherList;
